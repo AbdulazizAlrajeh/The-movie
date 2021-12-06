cfb936bbed002f089296477e91d4034e907535cd
AbdulazizAlrajeh <Abdulaziz.Alrajeh.dmm.k1@tuwaiq.edu.sa>
2021-12-06 16:52:10 +0300

    Finsh the splash for the app

---
+++ app/src/main/java/com/example/myapplication/views/SplashActivity.kt
@@ -0,0 +1,40 @@
+package com.example.myapplication.views
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.example.myapplication.R
+
+class SplashActivity : AppCompatActivity() {
+    private var mDelayHandler: Handler? = null
+    private val splashLong: Long = 5000 // five seconds
+    internal val mRunnable: Runnable = Runnable {
+        // for count the time
+        if (!isFinishing) {
+
+            val intent = Intent(applicationContext, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        mDelayHandler = Handler()
+
+        mDelayHandler!!.postDelayed(mRunnable, splashLong)
+    }
+
+
+    override fun onDestroy() {
+
+        if (mDelayHandler != null) {
+            mDelayHandler!!.removeCallbacks(mRunnable)
+        }
+
+        super.onDestroy()
+    }
+
+}
